7ae64c04cadd8cbeed69f51fb889b9f6ac2969a1
Cezary Karwoski-Budd <cezary.karwoski-budd@comparethemarket.com>
2025-06-25 17:08:29 +0100

    Card displays name and spacing between attributes

--- src/CardDetails.tsx
+++ src/CardDetails.tsx
@@ -3,21 +3,32 @@ import { Card } from "./classes/Card"
 export const CardDetails = ({ card }: { card: Card }) => {
     return (
         <div>
-            {card.extra ? [...Array(card.extra).keys()].map((_, index: number) => {
-                return <img key={index} src={'images/extra.png'} width='30rem' height='30rem'/>
-            }): null}
-            {card.heal ? [...Array(card.heal).keys()].map((_, index: number) => {
-                return <img key={index} src={'images/heal.png'} width='30rem' height='30rem'/>
-            }): null}
-            {card.attack ? [...Array(card.attack).keys()].map((_, index: number) => {
-                return <img key={index} src={'images/attack.png'} width='30rem' height='30rem'/>
-            }): null}
-            {card.draw ? [...Array(card.draw).keys()].map((_, index: number) => {
-                return <img key={index} src={'images/card.png'} width='30rem' height='30rem'/>
-            }): null}
-            {card.shield ? [...Array(card.shield).keys()].map((_, index: number) => {
-                return <img key={index} src={'images/shield_alive.png'} width='30rem' height='30rem'/>
-            }): null}
+            <p>{card.name}</p>
+            <div>
+                {card.extra ? [...Array(card.extra).keys()].map((_, index: number) => {
+                    return <img key={index} src={'images/extra.png'} width='30rem' height='30rem'/>
+                }): null}
+            </div>
+            <div>
+                {card.heal ? [...Array(card.heal).keys()].map((_, index: number) => {
+                    return <img key={index} src={'images/heal.png'} width='30rem' height='30rem'/>
+                }): null}
+            </div>
+            <div>
+                {card.attack ? [...Array(card.attack).keys()].map((_, index: number) => {
+                    return <img key={index} src={'images/attack.png'} width='30rem' height='30rem'/>
+                }): null}
+            </div>
+            <div>
+                {card.draw ? [...Array(card.draw).keys()].map((_, index: number) => {
+                    return <img key={index} src={'images/card.png'} width='30rem' height='30rem'/>
+                }): null}
+            </div>
+            <div>
+                {card.shield ? [...Array(card.shield).keys()].map((_, index: number) => {
+                    return <img key={index} src={'images/shield_alive.png'} width='30rem' height='30rem'/>
+                }): null}
+            </div>
         </div>
     )
 }
